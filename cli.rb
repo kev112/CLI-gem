7fa7c4203d8fa1e7dba275f727505183694a5212
kev112 <werewaswas@live.com>
2018-09-04 20:46:58 -0700

    Create cli.rb

---
+++ cli.rb
@@ -0,0 +1,15 @@
+Here are various cat breeds:
+1
+2
+3
+
+Which breed do you want to learn more about?
+
+
+Surmese:
+Bered: wefwef
+Fefwe: wefawefb
+Bwefwe: vawevwab
+
+Type ‘c’ and ENTER to see list of the cats again.
+‘x’ to exit
