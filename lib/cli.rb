9767deda465e02fefeb2df3cd087a8ed44abf903
kev112 <werewaswas@live.com>
2018-09-04 21:17:31 -0700

    get user input

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,33 +1,37 @@
 class CLI
-    def list_cats
+    def list_cats(url)
 
     end
 
     def get_input 
-    
+        input = gets
+        case input
+        when 'exit'
+            puts 'Exiting...Bye!'
+        end
     end
 
-    def show_cat
+    def show(cat)
 
     end
 
     def quit
-        
+
     end
 end
 
-Here are various cat breeds:
-1
-2
-3
+# Here are various cat breeds:
+# 1
+# 2
+# 3
 
-Which breed do you want to learn more about?
+# Which breed do you want to learn more about?
 
 
-Surmese:
-Bered: wefwef
-Fefwe: wefawefb
-Bwefwe: vawevwab
+# Surmese:
+# Bered: wefwef
+# Fefwe: wefawefb
+# Bwefwe: vawevwab
 
-Type ‘c’ and ENTER to see list of the cats again.
-‘x’ to exit
+# Type ‘cats’ and ENTER to see list of the cats again.
+# ‘exit’ to exit
